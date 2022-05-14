2968b5f1fa045729ec5328da760c0d5ce8d1d1ec
Daha <dahawong@gmail.com>
2022-05-14 10:45:49 +0800

    Ignore text messages via bot.

--- handlers/message.py
+++ handlers/message.py
@@ -5,7 +5,7 @@ import callbacks.message as callback
 username_handler = MessageHandler(filters.TEXT, callback.request_password)
 password_handler = MessageHandler(filters.TEXT, callback.verify_login)
 
-link_handler = MessageHandler(filters.Entity(
-    'url') | filters.Entity('text-link'), callback.save_link)
+link_handler = MessageHandler(~filters.VIA_BOT & (filters.Entity(
+    'url') | filters.Entity('text-link')), callback.save_link)
 normal_text_handler = MessageHandler(filters.TEXT & ~(filters.Entity(
     'url') | filters.Entity('text-link')), callback.reply_normal_text)
